23def9ad13ccdad050db3ecf9fb68cbbab14c31f
Mendel Hornbacher <Mendel Hornbacher>
2017-03-15 14:59:12 -0400

    Authroutes varified and documented

--- express/routes/auth.js
+++ express/routes/auth.js
@@ -3,6 +3,7 @@ var User = require('../app/models/user');
 var config = require("../config/main");
 var jwt = require('jsonwebtoken');
 var passport = require('passport');
+var morgan = require('morgan');
 
 //Create The Router
 var authRoutes = express.Router();
@@ -11,10 +12,10 @@ var authRoutes = express.Router();
 authRoutes.post('/authenticate', function (req, res) {
   //Find the user
   User.findOne({ email: req.body.email }, function (err, user) { //When found
-    if (err) throw err; //if there is an error burn the world why not?
+    if (err) morgan(err); //if there is an error burn the world why not?
 
     if (!user) {  //if there is no user send a letter home saying that it failed.
-      res.send({ success: false, message: 'Authentication failed.' });
+      res.send(403, { success: false, message: 'Authentication failed.' });
     } else {  //We have a correct username now...
       // Check if password matches
       user.comparePassword(req.body.password, function (err, isMatch) {
@@ -27,7 +28,7 @@ authRoutes.post('/authenticate', function (req, res) {
           res.json({ success: true, name: user.name, number: user.number, role: user.role, token: 'JWT ' + token });
         } else {
           // should we tell him the username is good?
-          res.send({ success: false, message: 'Authentication failed.' });
+          res.send(403, { success: false, message: 'Authentication failed.' });
         }
       });
     }
@@ -42,10 +43,10 @@ authRoutes.get('/users', function(req, res){
   if (req.user.role == "admin" || req.user.role == 'moderator'){
     User.find({}, { password: 0, __v: 0}, function(err, users){
       if (err) throw err;
-      res.json({success: true, message: "Successfull listing of users", users: users})
+      res.json(200, {success: true, message: "Successfull listing of users", users: users})
     })
   } else {
-    res.json({success: false, message: "Invalid Account Permissions"})
+    res.json(403, {success: false, message: "Invalid Account Permissions"})
   }
 });
 
@@ -58,48 +59,50 @@ authRoutes.put('/users', function (req, res) {
       User.findOne({ email: req.body.user }, { password: 0, __v: 0}, function (err, user) {  //find the user with that email address
         if (err) throw err;
         if (!user) {  // user does not exist
-          res.send({ success: false, message: 'User not found.' });
+          res.send(500, { success: false, message: 'User not found.' });
+        } else if(user.role == 'admin' && user._id != req.user._id){
+          res.json(403, {success: false, message: "Invalid Account Permissions"})
         } else {
           let field = req.body.field;
 
           if (field == 'name') {
             user.name = req.body.value;
             user.save(function (err) {
-              if (err) { return res.json({ success: false, message: 'Error updating name', error: err}); }
-              res.json({ success: true, message: 'Successfully updated name', user: user });
+              if (err) { return res.json(500, { success: false, message: 'Error updating name', error: err}); }
+              res.json(202, { success: true, message: 'Successfully updated name', user: user });
             });
           } else if (field == 'number') {
             user.number = req.body.value;
             user.save(function (err) {
-              if (err) { return res.json({ success: false, message: 'Error updating number', error: err}); }
-              res.json({ success: true, message: 'Successfully updated number', user: user });
+              if (err) { return res.json(500, { success: false, message: 'Error updating number', error: err}); }
+              res.json(202, { success: true, message: 'Successfully updated number', user: user });
             });
           } else if (field == 'password'){
             user.password = req.body.value;
             user.save(function (err) {
-              if (err) { return res.json({ success: false, message: 'Error updating password', error: err}); }
-              res.json({ success: true, message: 'Successfully updated password', user: user });
+              if (err) { return res.json(500, { success: false, message: 'Error updating password', error: err}); }
+              res.json(202, { success: true, message: 'Successfully updated password', user: user });
             });
           } else if (field == 'role'){
             if (user.schema.path('role').enumValues.indexOf(req.body.value) >= 0){
               user.role = req.body.value;
               user.save(function (err) {
-                if (err) { return res.json({ success: false, message: 'Error updating role', error: err }); }
-                res.json({ success: true, message: 'Successfully updated role', user: user });
+                if (err) { return res.json(500, { success: false, message: 'Error updating role', error: err }); }
+                res.json(202, { success: true, message: 'Successfully updated role', user: user });
               });
             } else {
-              res.json({ success: false, message: 'Invalid role' });
+              res.json(400, { success: false, message: 'Invalid role' });
             }
           } else {
-            res.json({ success: false, message: 'Invalid Field' });
+            res.json(400, { success: false, message: 'Invalid Field' });
           }
         }
       });
     } else {
-      res.json({success: false, message: "Please supply the user (email), field and value you want to edit"});
+      res.json(400, {success: false, message: "Missing header info"});
     }
   } else {
-    res.json({ success: false, message: "Invalid Account Permissions" })
+    res.json(403, { success: false, message: "Invalid Account Permissions" })
   }
 });
 
@@ -109,7 +112,7 @@ authRoutes.put('/users', function (req, res) {
 authRoutes.post('/users', function (req, res) {  //
   if (req.user.role == 'admin') {
     if (!req.body.email || !req.body.password || !req.body.name || !req.body.number) {
-      res.json({ success: false, message: 'Please submit email, password, name and number' })
+      res.json(400, { success: false, message: 'Missing Information', request_body: req.body })
     } else {
       var newUser = new User({
         email: req.body.email,
@@ -120,15 +123,13 @@ authRoutes.post('/users', function (req, res) {  //
 
       newUser.save(function (err) {
         if (err) {
-          return res.json({ success: false, message: 'That email address already exists.' });
+          return res.json(409, { success: false, message: 'That email address already exists.' });
         }
-        res.json({
-          success: true, message: 'Successfully created a new user.'
-        });
+        res.json(201, {success: true, message: 'Successfully created a new user.'});
       });
     };
   } else {
-    res.json({ success: false, message: 'Invalid Account Permissions', role: req.user.role });
+    res.json(403, { success: false, message: 'Invalid Account Permissions', current_role: req.user.role });
   };
 });
 
@@ -139,14 +140,18 @@ authRoutes.delete('/users', function(req, res){
   if (req.user.role == 'admin'){
     if (req.body.user){
       User.findOneAndRemove({email: req.body.user}, { password: 0, __v: 0}, function(err, user){
-        if(err) {return res.json({success: false, message: "Error getting user", user: req.body.user})};
-        res.json({success: true, message:"Successfully deleted user", user: user});
+        if(err) {return res.json(500, {success: false, message: "Error getting user", user: req.body.user})};
+        if (user === null){
+          return res.json(404, {success: false, message:"No Such User"});
+        } else {
+          res.json(200, {success: true, message:"Successfully deleted user", user: user});
+        }
       });
     } else{
-      res.json({ success: false, message: 'Please provide user', user: req.body.user });
+      res.json(400, { success: false, message: 'No User Given' });
     }
   } else {
-    res.json({ success: false, message: 'Invalid Account Permissions', role: req.user.role });
+    res.json(403, { success: false, message: 'Invalid Account Permissions', role: req.user.role });
   }
 });
 
