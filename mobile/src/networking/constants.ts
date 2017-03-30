6d97bd575edff3450b76e7da7d120bf645e28557
PCGeek <mendelh1537@gmail.com>
2017-03-30 12:03:28 -0400

    Fixed big bug

--- mobile/src/networking/constants.ts
+++ mobile/src/networking/constants.ts
@@ -1,3 +1,3 @@
-// export const URL = "https://www.chaverimch.com"
+export const URL = "https://www.chaverimch.com"
 // export const URL = "http://192.168.1.14:3000"
-export const URL = ""
+// export const URL = ""
